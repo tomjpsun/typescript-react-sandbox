758186bd0af48b0b168d0f1f5618762955694bdf
Tom Sun <tomjpsun@gmail.com>
2023-08-11 13:24:45 +0800

    project start with a router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import {
+  useNavigate,
+  createBrowserRouter,
+  RouterProvider,
+  Link,
+} from "react-router-dom";
+
+const Home = () => {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+    </header>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router} />
     </div>
   );
 }
+const About = () => {
+  const navigate = useNavigate();
+  const onClick = () => {
+    navigate("/");
+  };
+  return (
+    <div>
+      <button onClick={onClick}>BACK</button>
+    </div>
+  );
+};
 
+// add Home component to the following array, with path as 'home'
+const router = createBrowserRouter([
+  // show <h1> hello </h1> and a link to /about
+  {
+    path: "/",
+    element: (
+      <div>
+        <h1>hello</h1>
+        <Link to="/about">About</Link>
+      </div>
+    ),
+  },
+
+  {
+    path: "about",
+    element: <About></About>,
+  },
+  {
+    path: "home",
+    element: <Home></Home>,
+  },
+]);
 export default App;
