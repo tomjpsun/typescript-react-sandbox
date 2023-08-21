f5191000ba89e09586efaa0eef2830e6059fa28e
Tom Sun <tomjpsun@gmail.com>
2023-08-21 17:31:30 +0800

    define LogMessage interface

--- src/App.tsx
+++ src/App.tsx
@@ -2,39 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./scss/style.scss";
 import "./App.css";
-
+import myLogger from "./loggers/Logger";
 import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
 import About from "./About";
 
 const Home = () => {
-  const myfetch = () => {
-    const msg = {
-      id: "434a5f5e-2f5f-11ed-a261-0242ac120002",
-      datetime: "24/Jan/2023:14:12:15 +0000",
-      host: "153.10.110.81",
-      "user-identifier": "Mozilla/5.0 Gecko/20100101 Firefox/64.0",
-      method: "PUT",
-      status: 500,
-      referrer: "http://www.google.com/",
-    };
-    const dataToSend = JSON.stringify(msg);
-    fetch("http://localhost:8200/api/v1/ingest", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "X-P-META-meta1": "value1",
-        "X-P-TAG-tag1": "value1",
-        "X-P-Stream": "demo",
-        Authorization: "Basic YWRtaW46YWRtaW4=",
-      },
-      body: dataToSend,
-    }).then((response) => {
-      console.log("response", response);
-    });
-  };
-
   const handleClick = () => {
-    myfetch();
+    myLogger();
   };
   return (
     <header className="App-header">
---
+++ src/loggers/Logger.tsx
@@ -0,0 +1,36 @@
+interface LogMessage {
+  id: string;
+  datetime: string;
+  host: string;
+  "user-identifier": string;
+  level: number;
+  message: string;
+}
+const myLogger = () => {
+  //"24/Jan/2023:14:12:15 +0000"
+
+  const msg: LogMessage = {
+    id: "434a5f5e-2f5f-11ed-a261-0242ac120002",
+    datetime: Date.now().toString(),
+    host: "127.0.0.1",
+    "user-identifier": "Mozilla/5.0 Gecko/20100101 Firefox/64.0",
+    level: 100,
+    message: "default log message",
+  };
+
+  const dataToSend = JSON.stringify(msg);
+  fetch("http://localhost:8200/api/v1/ingest", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-P-META-meta1": "value1",
+      "X-P-TAG-tag1": "value1",
+      "X-P-Stream": "demo",
+      Authorization: "Basic YWRtaW46YWRtaW4=",
+    },
+    body: dataToSend,
+  }).then((response) => {
+    console.log("response", response);
+  });
+};
+export default myLogger;
